5aa5df3a80bfa9849a20ab6404df06f22ca0e729
Trevor Leung <trevortyleung@gmail.com>
2022-02-09 16:03:45 -0800

    added logo

--- client/App.jsx
+++ client/App.jsx
@@ -4,8 +4,8 @@ import { Outlet, Link, NavLink } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <h1 className="title">
-        C
+      <h1 className="title" style={{ paddingTop: "20px"}}>
+        Messenge
         <NavLink
           to="/secretPage"
           className="title"
@@ -13,10 +13,11 @@ const App = () => {
             isActive ? { display: "none" } : { textDecoration: "none" }
           }
         >
-          H
+          r 
         </NavLink>
-        AT ZONE
+         Pigeon
       </h1>
+      <img src={"./build/assets/pigeonlogo.png"} style={{ display:"block", marginLeft:"auto", marginRight:"auto", height:"150px", width:"auto" }}></img>
       <Outlet />
     </div>
   );
